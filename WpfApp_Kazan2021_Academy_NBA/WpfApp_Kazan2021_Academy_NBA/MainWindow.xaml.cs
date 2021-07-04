907b30d843c2c3a314c4b1e2203a679c45be170e
Gogs <gogs@fake.local>
2021-07-04 10:37:49 +0300

    WPF Starter Pack

--- WpfApp_Kazan2021_Academy_NBA/WpfApp_Kazan2021_Academy_NBA/MainWindow.xaml.cs
+++ WpfApp_Kazan2021_Academy_NBA/WpfApp_Kazan2021_Academy_NBA/MainWindow.xaml.cs
@@ -25,10 +25,10 @@ namespace WpfApp_Kazan2021_Academy_NBA
             InitializeComponent();
         }
 
-        //private void ButtonClicked(object sender, RoutedEventArgs e)
-        //{
-        //    BtnSecond.Content = "Hello, World!!!!!!";
-        //    BtnSecond.Background = Brushes.LightCoral;
-        //}
+        private void ButtonClicked(object sender, RoutedEventArgs e)
+        {
+            BtnSecond.Content = "Hello, World!!!!!!";
+            BtnSecond.Background = Brushes.LightCoral;
+        }
     }
 }
